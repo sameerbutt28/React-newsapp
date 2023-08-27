f7b800703ff6ebc45c5aa6223359486e1b6a5776
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-08-28 01:57:13 +0500

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+import './Navbar.css'
+
+// import PropTypes from 'prop-types'
+
+export class Navbar extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <nav className='navbar'>
+                    <ul className='nav_list'>
+                        <li > <Link to="/news"> NewsMonkey </Link>  </li>
+                        <li > <Link to="/news"> News </Link> </li>
+                        <li > <Link to="/about"> About </Link> </li>
+
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Navbar
