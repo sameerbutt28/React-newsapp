30783eded3dc82d0ab01a12b5d1dbdc7d44332f5
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-08-30 01:49:49 +0500

    Add files via upload

--- newsItem.js
+++ newsItem.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react'
 import './newsItem.css'
 
 export class newsItem extends Component {
+  
+
+  
   render() {
-    let {title, description} = this.props;
+    let {title, description, url} = this.props;
     return (
     
       <div className='card-container'>
         
         <div className='card' >
-  <img src="https://images.wsj.net/im-842273/social" alt="..." height= '100px'/>
+  <img src={url} alt="..." height= '160px' width='300px'/>
   <div >
-    <h5 className="card-title">{title}</h5>
+    <h3 className="card-title">{title}</h3>
     <p className="card-text">{description}</p>
-    <button> Read more </button>
+    <button>  </button>
   </div>
 </div>
 
