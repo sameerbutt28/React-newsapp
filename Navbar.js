f06a9663d0ad7187b86a8a1ea3825835b341545f
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-09-14 23:22:59 +0500

    Add files via upload

--- Navbar.js
+++ Navbar.js
@@ -16,8 +16,15 @@ export class Navbar extends Component {
                 <nav className='navbar'>
                     <ul className='nav_list'>
                         <li > <Link to="/news"> 28-News </Link>  </li>
-                        <li > <Link to="/news"> News </Link> </li>
+                        <li > <Link to="/news"> Top-Headlines </Link> </li>
                         <li > <Link to="/about"> About </Link> </li>
+                        <li > <Link to="/about"> Business </Link> </li>
+                        <li > <Link to="/about"> Entertainment</Link> </li>
+                        <li > <Link to="/about"> General</Link> </li>
+                        <li > <Link to="/about"> Health</Link> </li>
+                        <li > <Link to="/about"> Science</Link> </li>
+                        <li > <Link to="/about"> Sports</Link> </li>
+                        <li > <Link to="/about"> Technology</Link> </li>
 
                     </ul>
                 </nav>
